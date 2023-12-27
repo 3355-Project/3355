8360f55f16f609371a8887e0a4bea183b301c6d3
oneseel <seno1006@gmail.com>
2023-12-27 11:02:26 +0900

    init commit

---
+++ src/main/java/com/example/project3355/Project3355Application.java
@@ -0,0 +1,13 @@
+package com.example.project3355;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3355Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Project3355Application.class, args);
+  }
+
+}
