863c51df82c9f3c5b73a9d94524473d4c1ac4f52
Jeon <jeongsh950518@gmail.com>
2023-12-29 10:15:13 +0900

    보드만들때 수정 유저보드도 같이 저장할수잇게

--- src/main/java/com/example/project3355/coulmn/service/ColumnsService.java
+++ src/main/java/com/example/project3355/coulmn/service/ColumnsService.java
@@ -1,8 +1,10 @@
 package com.example.project3355.coulmn.service;
 
+import com.example.project3355.board.entity.UserBoard;
 import com.example.project3355.coulmn.dto.ColumnsRequestDto;
 import com.example.project3355.coulmn.dto.ColumnsResponseDto;
 import com.example.project3355.user.entity.User;
+import java.util.List;
 
 public interface ColumnsService {
 
@@ -13,4 +15,6 @@ public interface ColumnsService {
   void deleteColumns(Long id,User user);
 
   void sequenceColumns(Long boardId,Long id, Integer sequence,User user);
+
+  List<UserBoard> findMember(Long boardId,User user);
 }
