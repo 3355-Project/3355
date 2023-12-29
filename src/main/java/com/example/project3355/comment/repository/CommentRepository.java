5783889891334c88f60c6a8ad70f0e3975caf290
Jeon <jeongsh950518@gmail.com>
2023-12-29 18:14:36 +0900

    댓글 패키지로 정리

--- src/main/java/com/example/project3355/comment/repository/CommentRepository.java
+++ src/main/java/com/example/project3355/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
-package com.example.project3355.comment;
+package com.example.project3355.comment.repository;
 
 
+import com.example.project3355.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
